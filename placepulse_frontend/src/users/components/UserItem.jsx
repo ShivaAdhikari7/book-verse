fa9071fb24b77b9a2246730b9f99ed7dc0a9c52a
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2023-08-24 18:07:37 +0545

    complete design to display username with number of places added

--- placepulse_frontend/src/users/components/UserItem.jsx
+++ placepulse_frontend/src/users/components/UserItem.jsx
@@ -1,19 +1,25 @@
+import { Link } from "react-router-dom";
+
+import Avatar from "../../shared/UI/Avatar";
 import "./UserItem.css";
+import Card from "../../shared/UI/Card";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
