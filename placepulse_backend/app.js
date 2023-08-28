dd91dd7dc072225345f6fc53a63d0c7229748395
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2023-08-28 17:21:26 +0545

    error model for HttpError and no route found model

--- placepulse_backend/app.js
+++ placepulse_backend/app.js
@@ -2,12 +2,26 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
 const cors = require("cors");
-
+const HttpError = require("./models/http-error");
 require("./database/connection");
+
 app.use(express.json());
 app.use(cors());
 
 app.use("api/places", require("./routes/place-route"));
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
