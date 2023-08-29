a6ee306fbcf5e76ae61aa6488788325909863761
Shiva Adhikari <79002274+ShivaAdhikari7@users.noreply.github.com>
2023-08-29 09:21:28 +0545

    getUsers functionality

--- placepulse_backend/controllers/user-controller.js
+++ placepulse_backend/controllers/user-controller.js
@@ -4,6 +4,20 @@ const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 const User = require("../models/user-model");
 
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+};
+
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -143,4 +157,4 @@ const login = async (req, res, next) => {
   });
 };
 
-module.exports = { signup, login };
+module.exports = { signup, login, getUsers };
--- placepulse_backend/routes/users-routes.js
+++ placepulse_backend/routes/users-routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { check } = require("express-validator");
-const { signup, login } = require("../controllers/user-controller");
+const { signup, login, getUsers } = require("../controllers/user-controller");
 const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
+router.get("/", getUsers);
 router.post(
   "/signup",
   fileUpload.single("image"),
